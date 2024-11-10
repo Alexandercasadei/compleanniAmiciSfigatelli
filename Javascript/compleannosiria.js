029f4f7472b4b83bf2a6b3ac100c2a316d14bc3a
Alexandercasadei <99079553+Alexandercasadei@users.noreply.github.com>
2024-11-10 17:56:16 +0100

    Create compleannosiria.js

---
+++ Javascript/compleannosiria.js
@@ -0,0 +1,29 @@
+
+
+		var countDownDate = new Date("August 30, 2025 00:00:00").getTime();
+
+
+		var x = setInterval(function() {
+
+			
+			var now = new Date().getTime();
+
+
+			var distance = countDownDate - now;
+
+			
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+			
+			document.getElementById("countdown").innerHTML = "<span class='num'>" + days + "</span><span>giorni</span><span class='num'>" + hours + "</span><span>ore</span><span class='num'>" + minutes + "</span><span>minuti</span><span class='num'>" + seconds + "</span><span>secondi</span>";
+
+			
+			if (distance < 0) {
+				clearInterval(x);
+				document.getElementById("countdown").innerHTML = "Countdown finito auguri Siria";
+				countDownDate = new Date("August 30, " + (new Date().getFullYear() + 1) + " 00:00:00").getTime(); // Set the date for next year's countdown
+			}
+		}, 1000);
